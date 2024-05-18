97da14023b0f58be66ff0ea99e21fe601adb0be7
Jaime Perez Varas <jaime.perez.varas@gmail.com>
2024-05-17 21:25:56 -0400

    Creacion del AppDatabase

---
+++ app/src/main/java/perez/jaime/tdimplemtacionroom/model/bd/AppDatabase.kt
@@ -0,0 +1,9 @@
+package perez.jaime.tdimplemtacionroom.model.bd
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Usuario::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun usuarioDao() : UsuarioDao
+}
